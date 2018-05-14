6ca1808879635849f2b5de97997c33e92286b74f
micromanyth <miromanyth@gmail.com>
2018-05-14 14:23:37 -0400

    Add Post system tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
 	after_create :create_profile
 	has_many :subshreddits
-	has_many :posts, dependent: :destroy
-	has_many :comments, dependent: :destroy
+	has_many :posts
+	has_many :comments
 	has_one :profile, dependent: :destroy
 	has_many :messages, class_name: "Message", foreign_key: "recipient_id"
   has_many :sent_messages, class_name: "Message", foreign_key: "sender_id"
--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -2,9 +2,20 @@ require "application_system_test_case"
 
 class PostsTest < ApplicationSystemTestCase
   
+	def setup
+		@subshreddit = subshreddits(:one)
+		@user = users(:valid)
+		@post = Post.create!(subshreddit_id: @subshreddit.id, user_id: @user.id, title: 'sup', body: 'test')
+	end
+
   test "view index of all posts" do
     visit posts_path
     assert_selector "div", id: "main-posts"
+  end
+
+  test 'should show a single post' do
+  	visit post_path(@post)
+  	assert_selector "h2", id: "single-post"
   end
 
 end
